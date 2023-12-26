800394255c4deeb8aefdb202e49ca6fecdde4d70
Tafonso91 <t.veloso.afonso@ipvc.pt>
2023-12-26 16:31:09 +0000

    db_rel actual done

--- docker/images/db/rel/initial_schema.sql
+++ docker/images/db/rel/initial_schema.sql
@@ -4,90 +4,76 @@ CREATE EXTENSION IF NOT EXISTS POSTGIS_TOPOLOGY;
 
 -- Tabela Country
 CREATE TABLE public.country (
-    country_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    country_name VARCHAR(50)
+    id   uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    country_name VARCHAR(50),
+    geom GEOMETRY,
+    created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_on      TIMESTAMP NOT NULL DEFAULT NOW() 
 );
 
 -- Tabela Strong_Foot
 CREATE TABLE public.strong_foot (
-    foot_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    foot_name VARCHAR(50)
+    id  uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    foot_name VARCHAR(50),
+    created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- Tabela Club
 CREATE TABLE public.club (
-    club_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    club_name VARCHAR(100)
+    id  uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    club_name VARCHAR(100),
+    created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- Tabela Player
 CREATE TABLE public.player (
-    player_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    club_id INT,
-    country_id INT,
+    id    uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(100),
     height INT,
     price VARCHAR(20),
     salary VARCHAR(20),
-    foot_id INT,
-    FOREIGN KEY (club_id) REFERENCES Club(club_id),
-    FOREIGN KEY (country_id) REFERENCES Country(country_id),
-    FOREIGN KEY (foot_id) REFERENCES Strong_Foot(foot_id)
+    club_id uuid NOT NULL,
+    country_id uuid NOT NULL,
+    foot_id uuid NOT NULL,
+    created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- Tabela Player_Stats
 CREATE TABLE public.player_stats (
-    player_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id  uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     overall INT,
     potential INT,
     offense VARCHAR(10),
     defense VARCHAR(10),
-    crossing INT,
-    finishing INT,
-    heading INT,
-    short_pass INT,
-    volleys INT,
-    dribble INT,
-    curve INT,
-    free_kick INT,
-    long_pass INT,
-    ball_control INT,
-    acceleration INT,
-    sprint INT,
-    agility INT,
-    reactions INT,
-    balance INT,
-    shot_power INT,
-    jump INT,
-    stamina INT,
-    strength INT,
-    long_shot INT,
-    aggression INT,
-    positioning INT,
-    vision INT,
-    penalty INT,
-    composure INT,
-    interception INT,
-    defensive_awareness INT,
-    stand_tackle INT,
-    slide_tackle INT,
-    diving INT,
-    handling INT,
-    gk_kick INT,
-    gk_positioning INT,
-    reflexes INT,
-    FOREIGN KEY (player_id) REFERENCES Player(player_id)
-);
-
-
-/* Sample table and data that we can insert once the database is created for the first time */
-CREATE TABLE public.teachers (
-	name    VARCHAR (100),
-	city    VARCHAR(100),
-	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+    player_id uuid NOT NULL,
+    created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
+   
 );
 
-INSERT INTO teachers(name, city) VALUES('Luís Teófilo', 'Porto');
-INSERT INTO teachers(name, city) VALUES('Ricardo Castro', 'Braga');
+ALTER TABLE player
+    ADD CONSTRAINT player_club_id_fk
+        FOREIGN KEY (club_id) REFERENCES club
+            ON DELETE CASCADE;
+
+ALTER TABLE player
+    ADD CONSTRAINT player_country_id_fk
+        FOREIGN KEY (country_id) REFERENCES country
+            ON DELETE CASCADE;
+
+ALTER TABLE player
+    ADD CONSTRAINT player_strongfoot_id_fk
+        FOREIGN KEY (foot_id) REFERENCES strong_foot
+            ON DELETE CASCADE;
+
+ALTER TABLE player_stats
+    ADD CONSTRAINT playerstats_player_id_fk
+        FOREIGN KEY (player_id) REFERENCES player
+            ON DELETE CASCADE;
+
+
+
 
